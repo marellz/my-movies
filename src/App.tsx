006e12e7da65ab18923d0b44ad95dad632a25a69
Dave Njoroge <davenjoroge10@gmail.com>
2025-02-27 23:55:09 +0300

    basic app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [authenticated, setAuthenticated] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="max-w-4xl mx-auto h-screen flex flex-col">
+        <header className="py-4 flex">
+          <div className="flex-auto">
+            <h1 className="text-4xl">Welcome</h1>
+            <p className="text-gray-500">To your movie app.</p>
+          </div>
+          <div>
+            <button type="button">
+              Login
+            </button>
+          </div>
+        </header>
+
+        <main className="py-10 flex-auto">
+          {authenticated ? (
+            <h1>Authenticated!</h1>
+          ) : (
+            <h1>Not authenticated!</h1>
+          )}
+        </main>
+        <footer className="py-5">
+          <p>
+            Build by <a href="#dave">me</a>. Courtesy of TMDB.
+          </p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
